f407fce060cfcd8b8169a075e628c6178613753a
Cornelia Bea Persson <corneliap@live.se>
2021-04-10 11:28:16 +0200

    namnrefaktorering för tydlighet

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -15,8 +15,8 @@ import { ChooseOutfitPageComponent } from './garments/choose-outfit-page/choose-
 import { GarmentCardComponent } from './garments/choose-outfit-page/garment-card/garment-card.component';
 import { AttributesAlphabeticalPipe } from './pipes/attributes-alphabetical.pipe';
 import { DuplicateGarmentPipe } from './pipes/duplicate-garment.pipe';
-import { ChosenOutfitSidebarComponent } from './garments/choose-outfit-page/chosen-outfit/chosen-outfit-sidebar.component';
-import { ChosenOutfitGarmentCardComponent } from './garments/choose-outfit-page/chosen-outfit/chosen-garment-card/chosen-outfit-garment-card.component';
+import { ChosenOutfitSidebarComponent } from './garments/choose-outfit-page/chosen-outfit-sidebar/chosen-outfit-sidebar.component';
+import { ChosenOutfitGarmentCardComponent } from './garments/choose-outfit-page/chosen-outfit-sidebar/chosen-garment-card/chosen-outfit-garment-card.component';
 import { ManageGarmentsPageComponent } from './garments/manage-garments-page/manage-garments-page.component';
 import { ManageGarmentCardComponent } from './garments/manage-garments-page/manage-garment-card/manage-garment-card.component';
 import { EditGarmentPageComponent } from './garments/manage-garments-page/edit-garment-page/edit-garment-page.component';
